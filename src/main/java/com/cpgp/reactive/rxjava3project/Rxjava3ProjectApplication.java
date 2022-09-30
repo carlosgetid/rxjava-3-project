109a78f1e97bcf761b5e872428e811eb20d10313
playcar00 <carlospgp13@gmail.com>
2022-09-30 08:04:10 -0500

    chore: setup project

---
+++ src/main/java/com/cpgp/reactive/rxjava3project/Rxjava3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cpgp.reactive.rxjava3project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rxjava3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rxjava3ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cpgp/reactive/rxjava3project/Rxjava3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cpgp.reactive.rxjava3project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rxjava3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
